9be6114aedce2d0a1a00d10212b8d5815d219781
Comercial-GrupoSR <comercial.gruposr@gmail.com>
2026-07-31 08:47:19 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE_NAME = "app-cache-v1";
+
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  // Deixa as requisições passarem normalmente
+});
